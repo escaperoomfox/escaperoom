a28ee70e34a845a76b89bce13cafcf277c6f97c4
Stephen Lie Juliando Soetopo <SS37685X@cimbniaga.co.id>
2019-07-26 10:09:06 +0700

    first

---
+++ client/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD6ZCi0tnOJBPgdGxmfzvYfYbVnuscmLKY",
+  authDomain: "hockey-game-ab0b5.firebaseapp.com",
+  databaseURL: "https://hockey-game-ab0b5.firebaseio.com",
+  projectId: "hockey-game-ab0b5",
+  storageBucket: "hockey-game-ab0b5.appspot.com",
+  messagingSenderId: "192730912889",
+  appId: "1:192730912889:web:2a7ab0129caf7bbb"
+};
